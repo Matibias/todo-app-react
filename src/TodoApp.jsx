2a8031cf3d2ed5218099e39fa3216eb1fe1074e7
Matias Parra <mparra@retailsbs.com>
2023-03-27 21:09:23 -0300

    feat: add reducer

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,6 +1,25 @@
+import { useReducer } from "react"
+import { todoReducer } from "./helper/todoReducer"
+
+const initialState = [
+  {
+    id: new Date().getTime(),
+    description: 'Recolectar la piedra del alma',
+    done: false,
+  },
+  {
+    id: new Date().getTime() * 3,
+    description: 'Recolectar la piedra del poder',
+    done: false,
+  },
+]
 
 
 export const TodoApp = () => {
+
+  const [todos, dispatch] = useReducer( todoReducer, initialState )
+
+
   return (
     <>
       <h1>TodoApp</h1>
