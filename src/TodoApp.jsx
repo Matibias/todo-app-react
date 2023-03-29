0b29a43fa794ff4bcbb73e763b0f67b4a8477975
Matias Parra <mparrab103@gmail.com>
2023-03-28 23:08:06 -0300

    feat: persistence todo in local storage

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,24 +1,29 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 import { TodoAdd, TodoList } from "./components"
 import { todoReducer } from "./helper/todoReducer"
 
 const initialState = [
-  {
-    id: new Date().getTime(),
-    description: 'Recolectar la piedra del alma',
-    done: false,
-  },
-  {
-    id: new Date().getTime() * 3,
-    description: 'Recolectar la piedra del poder',
-    done: false,
-  },
+  // {
+  //   id: new Date().getTime(),
+  //   description: 'Recolectar la piedra del alma',
+  //   done: false,
+  // },
+ 
 ]
 
+const initTodos = () => {
+  return JSON.parse( localStorage.getItem('todos')) || [];
+}
 
 export const TodoApp = () => {
 
-  const [todos, dispatch] = useReducer( todoReducer, initialState )
+  const [todos, dispatch] = useReducer( todoReducer, initialState, initTodos )
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+
+  }, [todos])
+  
 
   const handleNewTodo = ( todo ) => {
 
