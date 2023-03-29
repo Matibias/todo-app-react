600f7b05c82306bced8cccd1aa01b6d8591d71f3
Matias Parra <mparra@retailsbs.com>
2023-03-28 22:49:33 -0300

    feat: save add todo in reducer

--- src/helper/todoReducer.js
+++ src/helper/todoReducer.js
@@ -2,8 +2,9 @@
 export const todoReducer = ( initialState = [], action ) => {
   
   switch (action.type) {
-    case 'ABC':
-        throw new Error('Action.type = ABC mp esta implementada');
+    case '[TODO] Add Todo':
+      return [ ...initialState, action.payload ]
+        
   
     default:
       return initialState;
