19f38322baaaff9aa53fd6602d970e1747100d44
Matias Parra <mparrab103@gmail.com>
2023-03-29 22:57:25 -0300

    refactor: useTodo

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,53 +1,10 @@
-import { useEffect, useReducer } from "react"
 import { TodoAdd, TodoList } from "./components"
-import { todoReducer } from "./helper/todoReducer"
-
-const initialState = [
-  // {
-  //   id: new Date().getTime(),
-  //   description: 'Recolectar la piedra del alma',
-  //   done: false,
-  // },
- 
-]
-
-const initTodos = () => {
-  return JSON.parse( localStorage.getItem('todos')) || [];
-}
+import { useTodo } from "./hooks"
 
 export const TodoApp = () => {
 
-  const [todos, dispatch] = useReducer( todoReducer, initialState, initTodos )
-
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos))
-
-  }, [todos])
+  const { todos, handleNewTodo, handleDeleteTodo, handleToggleTodo } = useTodo()
   
-
-  const handleNewTodo = ( todo ) => {
-    const action = {
-      type: '[TODO] Add Todo',
-      payload: todo
-    }
-
-    dispatch( action );
-  }
-
-  const handleDeleteTodo = (id) => {
-    dispatch({
-      type: '[TODO] Remove Todo',
-      payload: id
-    })
-  }
-
-  const handleToggleTodo = (id) => {
-    dispatch({
-      type: '[TODO] Toggle Todo',
-      payload: id
-    })
-  }
-
   return (
     <>
       <h1>TodoApp 10, <small>pendientes: 2</small></h1>
